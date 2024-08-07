c1dc3f9c72d2a2c6aa0205774a635659823db881
vishwajeet-4847 <exclusivebloh@gmail.com>
2024-08-07 17:15:15 +0530

    finalising automation testing with sending attachment in it

--- singleemail.py
+++ singleemail.py
@@ -1,19 +1,34 @@
 import yagmail as email
 import os 
+import pandas as pd
 
 
 sender = "sipuakk@gmail.com"
 reciever = "exclusivebloh@gmail.com"
 
 password = os.getenv("EMAIL_PASSWORD")
-
 subject = "this is for  automation testing 2"
 
-content = """
-    hello there!
-"""
-
+# create an SMTP object with the gmail credentials
 yag = email.SMTP(user = sender , password = password)
 
-yag.send(to = reciever, subject = subject, contents = content)
-print("sent")
+# read the csv file and convert it to a pandas dataframe
+
+data = pd.read_csv("details.csv")
+
+# convert the data to a dataframe for easy handling and iteration over rows
+df = pd.DataFrame(data)
+
+
+for index, row in df.iterrows():
+    content = [f"""
+        hello {row["name"]}! You have to pay
+        {row["amount"]}
+    """, row["filepath"]]
+    yag.send(to = row["email"], subject = subject, contents = content)
+    print(f"sent email to {row["email"]}")
+
+
+
+
+
